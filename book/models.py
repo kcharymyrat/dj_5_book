c5c379349ce9a420a4c09e469e6314740c6a5b81
Charymyrat Garryyev <kcharymyrat@gmail.com>
2022-07-07 14:42:21 +0500

    Book model was created

--- book/models.py
+++ book/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.CharField(max_length=255)
+    content = models.CharField(max_length=255, blank=True, null=True)
